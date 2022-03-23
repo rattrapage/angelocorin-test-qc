e78a78382846dce437d8c876046853dbfcfc437e
angelo <houngan.angelo@yahoo.com>
2022-03-23 17:44:00 +0100

    correction des erreurs

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -6,14 +6,25 @@
 
 namespace UPJV\Validator;
 
-class EstValide implements ValidatorInterface /**Validite interface
+class EstValide implements ValidatorInterface 
+/**
+ * Validite interface
+ */
 {
-    public function build(array $param): object /**construction parametre
+    public function build(array $param): object 
+	/**
+	*construction parametre
+	*/
     {
         return $this;
     }
 
-    public function check($input): bool * /**Verification
+    public function check($input): bool 
+	
+	/**
+	*Verification
+	*/
+	
     {
         return true;
     }
