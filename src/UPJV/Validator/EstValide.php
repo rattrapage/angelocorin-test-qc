1cdc5b1ad52b9ade0dc9747cf04ee88208539ee7
angelo <houngan.angelo@yahoo.com>
2022-03-29 15:21:18 +0200

    correction des erreurs

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -11,24 +11,24 @@ namespace UPJV\Validator;
  */
 class EstValide implements ValidatorInterface
 	{
-	/**
-	* Récupère la taille minimum spécifier dans la conf
-	*
-	* @param array $param
-	*
-	* @return object
-	*/
+/**
+* Récupère la taille minimum spécifier dans la conf
+*
+* @param array $param
+*
+* @return object
+*/
 public function build(array $param): object
 	{
      return $this;
 	}
-    /**
-     * Test la longueur de la chaine de caractères
-     *
-     * @param $input
-     *
-     * @return bool
-     */
+/**
+* Test la longueur de la chaine de caractères
+*
+* @param $input
+*
+* @return bool
+*/
     public function check($input): bool
     {
 	return true;
